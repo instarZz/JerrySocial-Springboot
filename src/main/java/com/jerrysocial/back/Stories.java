61d3b3d6e4cd9e32964f68bf611048002a57fffb
Jerome BERTRAND <jerome.bertrand@pouey.com>
2023-03-22 11:33:30 +0100

    Stories entity added

---
+++ src/main/java/com/jerrysocial/back/Stories.java
@@ -0,0 +1,33 @@
+package com.jerrysocial.back;
+
+public class Stories {
+
+	private Integer id;
+	public String title;
+	public String image;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+}
