d8c2a0e6334b77ff0ea359a80b369f874af2a483
Jerome BERTRAND <jerome.bertrand@pouey.com>
2023-03-22 11:26:20 +0100

    User entity added

---
+++ src/main/java/com/jerrysocial/back/User.java
@@ -0,0 +1,72 @@
+package com.jerrysocial.back;
+
+import java.util.Date;
+
+public class User {
+
+	public String username;
+	public String firstname;
+	public String lastname;
+	public String email;
+	public Date birthday;
+	private String password;
+	private Integer phoneNumber;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Date getBirthday() {
+		return birthday;
+	}
+	
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Integer getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(Integer phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+
+}
